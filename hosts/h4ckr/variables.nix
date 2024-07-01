cd8afea27e65d7e08fc707321b084f114581b52d
installer <installer@gmail.com>
2024-07-01 15:23:11 -0500

    Adding Flutter

---
+++ hosts/h4ckr/variables.nix
@@ -0,0 +1,23 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "cotw-fabier";
+  gitEmail = "dan@championsoftheweb.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "
+  monitor=HDMI-A-5, 2560x1440, 1920x0, 1
+  monitor=DP-3, 1920x1080, 0x360, 1
+  monitor=DP-4, 3440x1440, 0x1440, 1
+  ";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+
+  # Theme
+  theme = "atelier-lakeside";
+
+}
